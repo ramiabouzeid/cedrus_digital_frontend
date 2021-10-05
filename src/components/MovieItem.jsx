4545f92c181262189c777e0225e0abb407cb8583
Rami Abou Zeid <ramiabouzeid87@hotmail.com>
2021-10-05 22:04:54 +0400

    Add ability to change the vote

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
-import {Card, CardHeader, CardContent, Typography, Grid, Rating} from '@mui/material';
+import { useState } from 'react';
+import {Card, CardHeader, CardContent, Typography, Grid, Modal} from '@mui/material';
 import StarRating from './StarRating';
 
-
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const MovieItem = (props) => {
-
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
-        <Grid item xs={3}>
+        <Grid item md={3}>
             <Card>
                 <CardHeader
                     title={props.movie.title}
                     style={{"marginBottom":"0", "paddingBottom":"0"}}
                 />
                 <CardContent>
-                    <div style={{"paddingBottom": "57%","position":"relative", "marginBottom":"10px"}}>
-                    <iframe width="100%" height="100%" style={{"position":"absolute", "width":"100%", "left":"0", "top":"0"}} src={`https://www.youtube.com/embed/${props.movie.youtube_id}`} title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <div style={{"paddingBottom": "57%","position":"relative", "marginBottom":"10px"}} onClick={handleOpen}>
+                        <img width="100%" height="100%" style={{"position":"absolute", "width":"100%", "left":"0", "top":"0"}} alt="" src={`https://img.youtube.com/vi/${props.movie.youtube_id}/0.jpg`} />
                     </div>
                     <Typography variant="body2" color="text.secondary">
                     {props.movie.description}
@@ -23,6 +35,18 @@ const MovieItem = (props) => {
                     <StarRating votes={props.movie.votes} id={props.movie.id} />
                 </CardContent>
             </Card> 
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div style={style}>
+                    <div style={{"paddingBottom": "57%","position":"relative"}}>
+                    <iframe width="100%" height="100%" style={{"position":"absolute", "width":"100%", "left":"0", "top":"0"}} src={`https://www.youtube.com/embed/${props.movie.youtube_id}`} title="YouTube video player" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    </div>
+                </div>
+            </Modal>
         </Grid>
     )
 }
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState } from "react";
-import {Rating} from '@mui/material';
+import {Rating, Button} from '@mui/material';
 import { call_server } from '../utils/api';
 
 
@@ -20,24 +20,47 @@ const StarRating = (props) => {
             setVotesArray(votesArray => [...votesArray, data]);
         });
     }
+    const changeVote = (id) => {
+        console.log(id);
+        let api = '/votes/delete/'+id;
+        let method = 'DELETE';
+        let data = {}
+        call_server(method, api, data, true).then(data => {
+            console.log('data');
+            let filteredArray = votesArray.filter(item => item.id !== id)
+            setVotesArray(filteredArray);
+            //setVotesArray(votesArray => [...votesArray, data]);
+        });
+    }
     let votes = 0;
     let vote_by_me = false;
+    let my_vote = 0;
     for (var i = 0; i < votesArray.length; i++) {
         votes += parseFloat(votesArray[i].vote);
-        if(votesArray[i].user_id === user_id){
+        console.log(user_id);
+        if(parseInt(votesArray[i].user_id) === user_id){
+            console.log('matching');
             vote_by_me = true;
+            my_vote = votesArray[i];
         };
     }
     
     votes = votes/votesArray.length;
+    let button;
+    let style = {};
     if(vote_by_me){
-        return (
-            <div>You already voted</div>
-        )
+        button = <div>You voted: {my_vote.vote} star <Button onClick={() => changeVote(my_vote.id)}>Change my vote</Button></div>;
+        style = {
+            "pointerEvents": "none",
+            "opacity": "0.3"
+        }
     }
     return (
         <div>
+            {button}
+            <div>
             <Rating
+            style={style}
             name="hover-feedback"
             value={votes}
             precision={0.5}
@@ -45,6 +68,7 @@ const StarRating = (props) => {
                 changeRating(newValue);
             }}
             />
+            </div>
         </div>
 
     )
